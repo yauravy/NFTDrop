7b2e6ec7c93e58be62e549686910189aaa02eeff
yuyu <yauravy@gmail.com>
2022-04-03 17:29:29 +0200

    first commit

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+
+
+function NFTDropPage() {
+    //Auth
+    const connectWithMetamask = useMetamask();
+    const address = useAddress();
+    const disconnect = useDisconnect();
+    
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+        {/*Left */}
+        <div className="lg:col-span-4 bg-gradient-to-br from-orange-600 to-rose-500">
+            <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+                <div className="rounded-xl bg-gradient-to-br from-purple-400 to-yellow-400 p-2">
+                    <img 
+                        className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+                        src="https://links.papareact.com/8sg" 
+                        alt="" 
+                    />
+                </div>
+                <div className="space-y-2 p-5 text-center ">
+                    <h1 className="text-4xl font-bold text-white">
+                        SECRET APES
+                    </h1>
+                    <h2 className="text-xl text-gray-300">
+                        A Collection Of Secret Devs
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        {/*Right */}
+        <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+            {/* Header */}
+            <header className="flex items-center justify-between">
+                <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+                    The{' '} 
+                    <span className="font-extrabold underline decoration-red-500/50">Secret</span>{' '}
+                    Devs NFT
+                </h1>
+                <button 
+                    onClick={() => address ? disconnect() : connectWithMetamask()}
+                    className="rounded-full bg-orange-500 
+                    px-4 py-2 text-xs font-bold text-white
+                    lg:px-5 lg:py-3 lg:text-base"
+                >
+                    {address ? 'Sign Out' : 'Sign In'}
+                </button>
+            </header>
+            <hr className="my-2 border"/>
+            {address && (
+                <p className="text-center text-sm text-orange-500">
+                    You're logged in witth wallet {address.substring(0,5)}...{address.substring(address.length - 5)}
+                </p>
+            )}
+
+            {/* Content */}
+            <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
+                <img 
+                  className="w-80 object-cover pb-10 lg:h-40" 
+                  src="https://links.papareact.com/bdy" 
+                  alt="" 
+                />
+
+                <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The Secret Club Devs</h1>
+
+                <p className="pt-2 text-xl text-green-500">11 / 3300 NFT's claimed</p>
+            </div>
+
+            {/* Mint button */}
+            <button 
+              className="mt-10 h-16 w-full rounded-full 
+              bg-orange-600 text-white
+              font-bold"
+            >
+                Mint NFT (0.01 ETH)
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default NFTDropPage
